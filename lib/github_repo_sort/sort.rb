2370066644fabe74af5bb6f587315a8fb07f28c7
dkhamsing <dkhamsing@cirruspath.com>
2016-03-10 07:48:16 -0800

    isolate info

--- lib/github_repo_sort/sort.rb
+++ lib/github_repo_sort/sort.rb
@@ -1,39 +1,7 @@
 # Sorting
 module GitHubRepoSort
-  require 'octokit'
-  require 'netrc'
-  require 'parallel'
-  require 'ruby-progressbar'
-
-  NETRC_GITHUB_MACHINE = 'api.github.com'
-
+  require 'github_repo_sort/info'
   class << self
-    def info(c, list, name)
-      info = []
-      Parallel.each(list, :in_threads => 10, :progress => "Sorting") do |x|
-        x = x.sub 'https://github.com/', ''
-
-        begin
-          r = c.repo x
-        rescue => e
-          # puts "Error: #{e}"
-          yield e
-          next
-        end
-
-        i = {
-          'name' => x,
-          name => r[name],
-          'raw' => r
-        }
-
-        info.push i
-      end # parallel
-      # puts ''
-
-      info
-    end
-
     def sort(list, options=nil, compact=true)
       if options.nil?
         by = 'stargazers_count'
